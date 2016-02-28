bb4c671f3d8c7f1d39fc0dbd2a51d027e95c0db9
amit <amit6257@gmail.com>
2016-02-28 03:14:39 -0800

    adding alert dialog on button click

--- testApp/ButtonsViewController.m
+++ testApp/ButtonsViewController.m
@@ -13,12 +13,37 @@
 - (void) viewDidLoad {
     [self setTitle:@"ButtonsViewController"];
     self.view = [[UIView alloc]initWithFrame:[[UIScreen mainScreen] bounds]];
-    // cant use origin as 0,0???why???
-    UIButton* button = [[UIButton alloc]initWithFrame:CGRectMake(10, 50, 100, 100)];
-    [button setTitle:@"button" forState:UIControlStateNormal];
+    self.view.backgroundColor = [UIColor whiteColor];
     
-    self.view.backgroundColor = [UIColor blueColor];
-    [self.view addSubview:button];
+    //add button
+    [self.view addSubview:[self getButton]];
+    
+    //add button with dialog
+    UIButton* buttonWithAction = [self getButtonForAction];
+    [buttonWithAction addTarget:self action:@selector(showDialog:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:buttonWithAction];
 }
 
+- (UIButton*)getButton {
+    // cant use origin as 0,0???why???
+    UIButton* button = [[UIButton alloc]initWithFrame:CGRectMake(10, 100, 100, 30)];
+    [button setTitle:@"button" forState:UIControlStateNormal];
+    return button;
+}
+
+
+- (UIButton*)getButtonForAction {
+    // cant use origin as 0,0???why???
+    UIButton* button = [[UIButton alloc]initWithFrame:CGRectMake(10, 150, 100, 30)];
+    [button setTitle:@"press me" forState:UIControlStateNormal];
+    return button;
+}
+
+-(void)showDialog:(id)sender {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Alert title" message:@"Alert message" delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    
+    [alert show];
+}
 @end
