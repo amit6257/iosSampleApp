f93dd658074ed65060ef2fcb0458e7b60e1f80dc
Amit Agarwal <amitagarwal@amits-macbook-pro.local>
2016-02-27 21:53:25 -0800

    Initial Commit

---
+++ testApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Amit Agarwal on 2/27/16.
+//  Copyright © 2016 Amit Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Amit Agarwal on 2/27/16.
+//  Copyright © 2016 Amit Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
